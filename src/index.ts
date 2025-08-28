bbb7f6ddd57ec0acd8e9dcdaf9b5a95488897e66
Matt Blank <mattblank11@gmail.com>
2025-08-28 16:50:41 -0400

    Add wrapper client that accepts a u+p and gets a token

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * as phenoml from "./api/index.js";
 export { phenomlError, phenomlTimeoutError } from "./errors/index.js";
-export { phenomlClient } from "./Client.js";
+export { PhenoMLClient } from "./WrapperClient.js";
 export { phenomlEnvironment } from "./environments.js";
