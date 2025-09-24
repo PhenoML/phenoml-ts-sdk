f6ba5d3fdc5be7cdc19b06be81bb825833626c80
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-24 00:32:59 +0000

    SDK regeneration

--- src/api/resources/authtoken/client/Client.ts
+++ src/api/resources/authtoken/client/Client.ts
@@ -11,9 +11,9 @@ export declare namespace Authtoken {
         environment?: core.Supplier<environments.phenomlEnvironment | string>;
         /** Specify a custom URL to connect the client to. */
         baseUrl?: core.Supplier<string>;
-        token: core.Supplier<core.BearerToken>;
+        token?: core.Supplier<core.BearerToken | undefined>;
         /** Additional headers to include in requests. */
-        headers?: Record<string, string | core.Supplier<string | undefined> | undefined>;
+        headers?: Record<string, string | core.Supplier<string | null | undefined> | null | undefined>;
         fetcher?: core.FetchFunction;
     }
 }
@@ -22,7 +22,7 @@ export class Authtoken {
     protected readonly _options: Authtoken.Options;
     protected _auth: Auth | undefined;
 
-    constructor(_options: Authtoken.Options) {
+    constructor(_options: Authtoken.Options = {}) {
         this._options = _options;
     }
 
